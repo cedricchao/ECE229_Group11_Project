d3c8545ed1cfd540799f7593be824958245fa05e
Zeyun Wu <zywu1010@gmail.com>
2022-05-30 12:22:34 -0700

    allow multiple dep; add Filter; add exmaple

--- filter.py
+++ filter.py
@@ -48,17 +48,66 @@ def recommend_instr_filter(df, ratio):
 
 
 def department_filter(df, dep):
-    department = np.unique([c.split()[0] for c in df['course']])
-    if dep not in department:
+    '''
+    dep: list of string
+    '''
+    dep = set(dep)
+
+    department = set(np.unique([c.split()[0] for c in df['course']]))
+    if not dep.issubset(department):
         raise Exception("Department does not exist")
     
-    return df[[c.split()[0] == 'CSE' for c in df['course']]]
+    return df[[c.split()[0] in dep for c in df['course']]]
         
+class Filter():
+    def __init__(self, letter=None, gpa=None, time=None, rec_class=None, rec_instr=None, dep=None):
+        '''
+        letter: string
+        gpa: float
+        time: float
+        rec_class: float [0, 1]
+        rec_instr: float [0, 1]
+        dep: list of string
+        '''
+        assert isinstance(dep, list)
+
+        self.letter = letter 
+        self.gpa = gpa 
+        self.time = time 
+        self.rec_class = rec_class 
+        self.rec_instr = rec_instr 
+        self.dep = set(dep)
+
+    def run(self, df):
+        if self.letter:
+            df = letter_grade_filter(df, self.letter)
+        
+        if self.gpa:
+            df = gpa_filter(df, self.gpa)
+
+        if self.time:
+            df = time_filter(df, self.time)
+
+        if self.rec_class:
+            df = recommend_class_filter(df, self.rec_class)
+
+        if self.rec_instr:
+            df = recommend_instr_filter(df, self.rec_instr)
+
+        if self.dep:
+            df = department_filter(df, self.dep)
+
+        return df
 
 
 if __name__ == '__main__':
 
-    df = pd.read_excel('data/data.xlsx')
+    # an example of using filter
+    df = pd.read_excel('../data/data.xlsx')
     df = df.drop(['Unnamed: 0'], 1)
 
-    #
+    # would like to filter out any CSE or ECE courses
+    # that has less than 5 hours / week working hours
+    # has a better letter grade than B+
+    f = Filter(dep=['CSE', 'ECE'], time=5, letter='B+')
+    print(f.run(df))
