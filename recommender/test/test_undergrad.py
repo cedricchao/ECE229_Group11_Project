2f183948bc494b9d7ac6844f66a25caa3873e289
sumukhbadam <ssbadam@ucsd.edu>
2022-06-06 17:30:42 -0700

    added changes for pytest

---
+++ recommender/test/test_undergrad.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+from recommender.undergrade_recommendation import Planner
+from recommender.dataobj import coursereq
+
+def test_course_list():
+    """
+    test if the recommendation for a key is from the department it is provided only
+    """
+    plan = Planner()
+    out = plan.get_couse_list(['ECE'],'signal')
+    for cou in out:
+        assert 'ECE' in cou.split()[0]
+
+def test_planner():
+    """
+    test the planner
+    """
+    courses=[
+    coursereq(GPA_Actual=4,Time=(10/10)*4,Rcmnd_Instr=4,Recommend_Course=3.6,department=['ECE','CSE'],keyword='Digital signal Processing'),
+    coursereq(GPA_Actual=3,Time=(5/10)*4,Rcmnd_Instr=4,Recommend_Course=3.6,department=['ECE','CSE','MATH'],keyword='Machine Learning'),
+    coursereq(GPA_Actual=3.5,Time=(10/10)*4,Rcmnd_Instr=4,Recommend_Course=3.6,department=['ECE','CSE','MATH'],keyword='Optimization')]
+    planner = Planner()
+    output = planner.get_recommendation(courses)
+    assert len(output)>=0 
+    if len(output)>0:
+        assert isinstance(output[0],dict)
+
+    
+
+    
+
+
+
