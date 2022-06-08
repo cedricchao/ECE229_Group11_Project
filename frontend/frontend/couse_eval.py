baa181b396a448a50f5a9f0b1c59bbab8688cbb3
sumukhbadam <ssbadam@ucsd.edu>
2022-06-08 11:10:40 -0700

    adding test for course eval.py

--- frontend/frontend/couse_eval.py
+++ frontend/frontend/couse_eval.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from typing import List
 import os
-from dataobj import bar,bardata
+from frontend.dataobj import bar,bardata
 import re
 from collections import defaultdict
 
